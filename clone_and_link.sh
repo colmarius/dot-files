07b75c88197bb5b4e4a7347d8db87f034e3b5d92
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-03-01 12:27:51 +0100

    Add clone_and_link.sh script.

---
+++ clone_and_link.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pushd "$HOME"
+
+  if [ -d ".dot-files" ]; then
+    pushd ".dot-files"
+      git smart-pull
+    popd
+  else
+    git clone "git://github.com/colmarius/dot-files.git" ".dot-files"
+  fi
+
+  ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
+    [ "$f" == '.dot-files/files/.' ] ||
+    [ "$f" == '.dot-files/files/..' ] ||
+    [ "$f" == '.dot-files/files/.git' ] ||
+    ln -vsf "$f" .
+  done
+
+popd
